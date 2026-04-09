f9d35e0c372342226ada3c4eb24d6fc2f10bd5a6
XDFRSTW <santimyxe@gmail.com>
2026-04-09 17:49:31 +0200

    Homes

--- projecto/fronted/src/App.jsx
+++ projecto/fronted/src/App.jsx
@@ -3,7 +3,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router"
 // Este es el archivo css que usaremos para darleestilos a la página (tailwind es usado en pocas ocasiones)
 import './index.css'
 // Página de inicio
-import Home from './components/home';
+import Home from './components/Homes';
 // Página de iniciar sesión o registrarse
 import RegLog from './components/RegLog';
 import Perfil from './components/Perfil';
@@ -17,7 +17,7 @@ function App() {
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="/" element={<RegLog />} />
-          <Route path="/Perfil" element={<Perfil />} />
+          <Route path="/perfil" element={<Perfil />} />
         </Routes>
       </Router>
     </>
