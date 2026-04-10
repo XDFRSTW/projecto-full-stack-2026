9279bffe295718f8a76e46117efd9debe2fb7c0e
XDFRSTW <santimyxe@gmail.com>
2026-04-10 09:22:52 +0200

    nooooooooooo

--- projecto/fronted/vite.config.js
+++ projecto/fronted/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.FRONT_UNI || "http://localhost:5173"
 })
